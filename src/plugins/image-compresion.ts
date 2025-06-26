3c991b767822f0904f3871ed17c318f88b0c7068
hrondon272 <hansrondon1998@gmail.com>
2025-06-26 12:26:39 -0500

    Feat - nuevo paquete para convertir Heic a jpeg y ajustes en la compresión

--- src/plugins/image-compresion.ts
+++ src/plugins/image-compresion.ts
@@ -1,17 +1,33 @@
 import imageCompression from 'browser-image-compression';
+import { isHeic, heicTo } from "heic-to"
+
 
 export const compressImages = async (file: File) => {
+    let newFile = file;
+
+    if (await isHeic(file)) {
+        const jpeg = await heicTo({
+            blob: file,
+            type: "image/jpeg",
+            quality: 0.95
+        });
+        newFile = new File([jpeg], file.name.replace(/\.heic$/i, '.jpg'), {
+            type: "image/jpeg",
+            lastModified: Date.now(),
+        });
+    }
+
     const options = {
-        maxSizeMB: 1,
-        maxWidthOrHeight: 1000,
+        maxSizeMB: 5,
+        maxWidthOrHeight: 5000,
         useWebWorker: true,
     };
 
     try {
-        const compressedBlob = await imageCompression(file, options);
+        const compressedBlob = await imageCompression(newFile, options);
         return new File([compressedBlob], file.name, {
-            type: file.type,
-            lastModified: Date.now(),
+            type: newFile.type,
+            lastModified: newFile.lastModified,
         });
     } catch (error) {
         console.error('Error al comprimir la imagen:', error);
